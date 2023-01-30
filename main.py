6fc0bae2c18c8d0ee5a0dc27cdd071a8769d3899
CarolBarba <123923170+CarolBarba@users.noreply.github.com>
2023-01-30 07:55:27 +0200

    Create main.py for celsius_to_fahrenheit

---
+++ main.py
@@ -0,0 +1,7 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = celsius_to_fahrenheit(celsius)
+print("Temperature in Fahrenheit: {:.2f}".format(fahrenheit))
